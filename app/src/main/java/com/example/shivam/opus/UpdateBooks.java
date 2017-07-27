2bb72bb65a5e356a4d86d5bd86a2600299639ca3
Shivam <shivamsri1127@gmail.com>
2017-07-27 15:00:20 +0530

    commit (10)

--- app/src/main/java/com/example/shivam/opus/UpdateBooks.java
+++ app/src/main/java/com/example/shivam/opus/UpdateBooks.java
@@ -1,7 +1,10 @@
 package com.example.shivam.opus;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -11,11 +14,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class UpdateBooks extends AppCompatActivity {
+import com.example.shivam.opus.dbutil.OCons;
+import com.example.shivam.opus.dbutil.OHelp;
+import com.example.shivam.opus.dbutil.OMng;
 
+import java.util.List;
+
+public class UpdateBooks extends AppCompatActivity implements Spinner.OnItemSelectedListener{
+
+    SQLiteDatabase sb;
+    OMng o;
+    Spinner c;
+    String label;
+    EditText e1,e2,e3;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         //hide notification/status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -25,7 +44,76 @@ public class UpdateBooks extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.custom_ab);
 
         setContentView(R.layout.activity_update_books);
+        o=new OMng(this);
+        sb=o.open();
+
+        e1=(EditText)findViewById(R.id.txtbid);
+        e2=(EditText)findViewById(R.id.txtbname);
+        e3=(EditText)findViewById(R.id.txttcopy);
+        c=(Spinner)findViewById(R.id.spcat);
+        c.setOnItemSelectedListener(this);
+        loadSpinnerData();
+
+
     }
+    public void loadSpinnerData(){
+        OHelp db = new OHelp(getApplicationContext(),OCons.DB_Name,null,OCons.DB_Version);
+
+        // Spinner Drop down elements
+        List<String> lables = db.getAllLabels();
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, lables);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        c.setAdapter(dataAdapter);
+
+    }
+    public void updateBooks(View v){
+        String id = e1.getText().toString();
+        String name = e2.getText().toString();
+        String tcopies = e3.getText().toString();
+        String exp[] = {OCons.CId};
+        String args[] = {label};
+        String b = null;
+        int existT=0,existR=0;
+        int total=Integer.parseInt(tcopies);//from edittext
+         Cursor c=sb.rawQuery("SELECT TotalCopies,RemainingCopies FROM Book WHERE BookID=\""+id+"\"",null);
+        if (c != null && c.moveToFirst()) {
+            existT=c.getInt(c.getColumnIndex(OCons.BTCopies));
+            existR=c.getInt(c.getColumnIndex(OCons.BRCopies));
+            c.close();
+
+        }
+        int newTotal=total+existT;
+        int newRemain=newTotal+existR;
+        String newT=String.valueOf(newTotal);
+        String newR=String.valueOf(newRemain);
+        Toast.makeText(this, "original total="+existT+" original remaining="+existR, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "New Total="+newT+" new Remain="+newR, Toast.LENGTH_SHORT).show();
+
+       Cursor c1 = sb.rawQuery("SELECT CatID FROM Category WHERE CatName=\""+label+"\"", null);
+        //Cursor c = sb.query(OCons.CTable,exp,OCons.CName,args,null,null,null);
+        if (c1 != null && c1.moveToFirst()) {
+            b = c1.getString(c1.getColumnIndex(OCons.CId));
+            c1.close();
+        }
+        ContentValues cv=new ContentValues();
+        String ar[]={id};
+        cv.put(OCons.BName,name);
+        cv.put(OCons.BTCopies,newT);
+        cv.put(OCons.BRCopies,newR);
+        cv.put(OCons.BCId,b);
+        long l=sb.update(OCons.BTable,cv,OCons.BId+"=?",ar);
+        if(l>0)
+            Toast.makeText(this, "Updated", Toast.LENGTH_SHORT).show();
+    }
+
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     public void done(View v){
         Intent i=new Intent(this,Main.class);
@@ -51,4 +139,17 @@ public class UpdateBooks extends AppCompatActivity {
         AlertDialog adb = ad.create();
         adb.show();
     }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        label = parent.getItemAtPosition(position).toString();
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "You selected: " + label,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
+//Remaining_copies = Remaining_copies + Updated_total_Copies. And Total_Copies = Total_Copies + Updated_Total_Copies
--- app/src/main/java/com/example/shivam/opus/UpdateMembers.java
+++ app/src/main/java/com/example/shivam/opus/UpdateMembers.java
@@ -1,7 +1,9 @@
 package com.example.shivam.opus;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -11,9 +13,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.shivam.opus.dbutil.OCons;
+import com.example.shivam.opus.dbutil.OMng;
 
 public class UpdateMembers extends AppCompatActivity {
-
+    OMng o;
+    SQLiteDatabase sb;
+    EditText e1,e2,e3,e4,e5,e6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +34,34 @@ public class UpdateMembers extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.custom_ab);
 
         setContentView(R.layout.activity_update_members);
+        o = new OMng(this);
+        sb = o.open();
+
+        e1 = (EditText) findViewById(R.id.txtmemid);
+        e2 = (EditText) findViewById(R.id.txtname);
+        e3 = (EditText) findViewById(R.id.txtemail);
+        e4 = (EditText) findViewById(R.id.txtaddress);
+        e5 = (EditText) findViewById(R.id.txtphone);
+        e6 = (EditText) findViewById(R.id.txtdob);
+    }
+    public void update(View v){
+        String id=e1.getText().toString();
+        String name=e2.getText().toString();
+        String email=e3.getText().toString();
+        String address=e4.getText().toString();
+        String phone=e5.getText().toString();
+        String dob=e6.getText().toString();
+        ContentValues cv= new ContentValues();
+        cv.put(OCons.MName,name);
+        cv.put(OCons.MEmail,email);
+        cv.put(OCons.MAdd,address);
+        cv.put(OCons.MPhN,phone);
+        cv.put(OCons.MDob,dob);
+        String args[]={id};
+        int r=sb.update(OCons.MTable,cv,OCons.MId+"=?",args);
+        if(r>0)
+            Toast.makeText(this, "Updated", Toast.LENGTH_SHORT).show();
+
     }
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     public void done(View v){
